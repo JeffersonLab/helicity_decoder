d38f1d49d019dba60f6ccc25d3fe066000930e7d
Bryan Moffit <moffit@jlab.org>
2023-10-05 17:01:06 -0400

    Finish procedure calls for rest of test.  Add readline menu.

--- test/hdTestVXS.c
+++ test/hdTestVXS.c
@@ -15,106 +15,187 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "jvme.h"
-#include "dmaPList.h"
 #include "hdLib.h"
 #include "tiLib.h"
 #include "sdLib.h"
+
+char *progname;
+//
+//  Just check status of PLL for system and local clock
+//
 
 int32_t
 testClock()
 {
   int32_t rval = OK;
+  printf("%s: ----------------------------------------------------------------------\n", __func__);
 
   int32_t system = 0, local = 0;
   rval = hdGetClockPLLStatus(&system, &local);
 
-  printf("%s:   system = %d   local = %d\n",
-	 __func__, system, local);
+  printf("%s:\n\t system = %d (%s)\n\t local = %d (%s)\n",
+	 __func__,
+	 system, (system==1) ? "LOCKED" : "NOT LOCKED",
+	 local, (local==1) ? "LOCKED" : "NOT LOCKED");
 
   return rval;
 }
+
+//
+// TI Sends SyncReset 100 times... verify receiving them in the scalers
+//
 
 int32_t
 testSyncReset()
 {
   int32_t rval = OK;
   volatile uint32_t scalers[9];
+  printf("%s: ----------------------------------------------------------------------\n", __func__);
 
   if(hdReadScalers(scalers, 1))
     {
-      printf("  Signal scalers:\n");
-      printf("    Trig1            = 0x%08x (%d)\n", scalers[4], scalers[4]);
-      printf("    Trig2            = 0x%08x (%d)\n", scalers[5], scalers[5]);
-      printf("    SyncReset        = 0x%08x (%d)\n", scalers[6], scalers[6]);
+      printf("%s:\n\t SyncReset = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[6], scalers[6]);
+    }
+
+  int32_t isync;
+  for(isync = 0; isync < 100; isync++)
+    tiSyncReset(0);
+
+  if(hdReadScalers(scalers, 1))
+    {
+      printf("%s:\n\t SyncReset = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[6], scalers[6]);
     }
 
   return rval;
 }
+
+//
+// Enable Forced BUSY, check that it is propagated to the TI
+//
 
 int32_t
 testBusyOut()
 {
   int32_t rval = OK;
   uint8_t status = 0, ti_status = 0;
-
+  printf("%s: ----------------------------------------------------------------------\n", __func__);
   hdBusy(0);
   hdBusyStatus(&status);
   ti_status = tiGetSWBBusy(0);
 
-  printf("%s:   Busy off     status = %d     ti_status = %d\n",
-	 __func__, status, ti_status);
+  printf("%s:\n\t Busy off     status = %d     ti_status = %d\n",
+	 __func__,
+	 status, ti_status);
 
   hdBusy(1);
   hdBusyStatus(&status);
   ti_status = tiGetSWBBusy(0);
 
-  printf("%s:   Busy ON      status = %d     ti_status = %d\n",
-	 __func__, status, ti_status);
+  printf("%s:\n\t Busy ON      status = %d     ti_status = %d\n",
+	 __func__,
+	 status, ti_status);
 
   hdBusy(0);
 
   return rval;
 }
+
+//
+// TI Sends trig1 1000 times... verify receiving them in the scalers
+//
 
 int32_t
 testTrigger1()
 {
   int32_t rval = OK;
   volatile uint32_t scalers[9];
+  printf("%s: ----------------------------------------------------------------------\n", __func__);
 
   if(hdReadScalers(scalers, 1))
     {
-      printf("  Signal scalers:\n");
-      printf("    Trig1            = 0x%08x (%d)\n", scalers[4], scalers[4]);
-      printf("    Trig2            = 0x%08x (%d)\n", scalers[5], scalers[5]);
-      printf("    SyncReset        = 0x%08x (%d)\n", scalers[6], scalers[6]);
+      printf("%s:\n\t Trig1 = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[4], scalers[4]);
     }
+
+  tiSoftTrig(1, 1000, 100, 0);
+
+  if(hdReadScalers(scalers, 1))
+    {
+      printf("%s:\n\t Trig1 = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[4], scalers[4]);
+    }
+
 
   return rval;
 }
+
+//
+// TI Sends trig2 1000 times... verify receiving them in the scalers
+//
 
 int32_t
 testTrigger2()
 {
   int32_t rval = OK;
   volatile uint32_t scalers[9];
+  printf("%s: ----------------------------------------------------------------------\n", __func__);
 
   if(hdReadScalers(scalers, 1))
     {
-      printf("  Signal scalers:\n");
-      printf("    Trig1            = 0x%08x (%d)\n", scalers[4], scalers[4]);
-      printf("    Trig2            = 0x%08x (%d)\n", scalers[5], scalers[5]);
-      printf("    SyncReset        = 0x%08x (%d)\n", scalers[6], scalers[6]);
+      printf("%s:\n\t Trig2 = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[5], scalers[5]);
     }
+
+  tiSoftTrig(2, 1000, 100, 0);
+
+  if(hdReadScalers(scalers, 1))
+    {
+      printf("%s:\n\t Trig2 = 0x%08x (%d)\n",
+	     __func__,
+	     scalers[5], scalers[5]);
+    }
+
 
   return rval;
 }
+
+#include <readline/readline.h>
+int com_quit(char *arg);
+int com_help(char *arg);
+
+typedef struct
+{
+  char *name;			/* User printable name of the function. */
+  rl_icpfunc_t *func;		/* Function to call to do the job. */
+  char *doc;			/* Documentation for this function.  */
+} COMMAND;
+
+COMMAND commands[] = {
+  {"help", com_help, "Display this text"},
+  {"?", com_help, "Synonym for `help'"},
+  {"clock", testClock, "Check Clock PLL Lock"},
+  {"syncreset", testSyncReset, "Check SYNCRESET"},
+  {"busy", testBusyOut, "Test BUSYOUT"},
+  {"trig1", testTrigger1, "Check TRIG1"},
+  {"trig2", testTrigger2, "Check TRIG2"},
+  {"quit", com_quit, "Quit"},
+  {(char *) NULL, (rl_icpfunc_t *) NULL, (char *) NULL}
+};
+#include "readline_menu.h"
 
 int
 main(int argc, char *argv[])
 {
   int stat;
   uint32_t address=0;
+  progname = dupstr(argv[0]);
 
   if (argc > 1)
     {
@@ -137,12 +218,14 @@ main(int argc, char *argv[])
 
   // TI init + config
   stat = tiInit(0,0,0);
+
+  // resets required before syncs and trigs
   tiClockReset();
   taskDelay(1);
   tiTrigLinkReset();
   taskDelay(1);
-  tiEnableVXSSignals();
 
+  // sd init
   stat = sdInit(0);
 
   // helicity decoder init + config
@@ -152,8 +235,15 @@ main(int argc, char *argv[])
   hdGetSlotNumber(&hd_slotnumber);
   hdStatus(1);
 
+  // Enable BUSY from helicity decoder to TI
   sdSetActiveVmeSlots(1 << hd_slotnumber);
   sdStatus(1);
+
+  initialize_readline(progname);	/* Bind our completer. */
+  com_help("");
+  strcat(progname, ": ");
+  readline_menu_loop(progname);
+
 
  CLOSE:
 
@@ -169,68 +259,6 @@ main(int argc, char *argv[])
   exit(0);
 }
 
-#if 0
-void
-hdReadout()
-{
-  DMA_MEM_ID vmeIN,vmeOUT;
-  extern DMANODE *the_event;
-  extern unsigned int *dma_dabufp;
-  DMANODE *outEvent;
-  int32_t idata, dCnt;
-
-  dmaPFreeAll();
-  vmeIN  = dmaPCreate("vmeIN",10240,1,0);
-  vmeOUT = dmaPCreate("vmeOUT",0,0,0);
-
-  dmaPStatsAll();
-
-  dmaPReInitAll();
-
-  int timeout=0;
-  while((hdBReady(0)!=1) && (timeout<100))
-    {
-      timeout++;
-    }
-
-  if(timeout>=100)
-    {
-      printf("TIMEOUT!\n");
-      goto CLOSE;
-    }
-
-  GETEVENT(vmeIN,1);
-
-  vmeDmaConfig(2,5,1);
-  dCnt = hdReadBlock(dma_dabufp, 1024>>2,1);
-  if(dCnt<=0)
-    {
-      printf("No data or error.  dCnt = %d\n",dCnt);
-    }
-  else
-    {
-      dma_dabufp += dCnt;
-    }
-
-  PUTEVENT(vmeOUT);
-
-  outEvent = dmaPGetItem(vmeOUT);
-
-  dCnt = outEvent->length;
-
-  printf("  dCnt = %d\n",dCnt);
-  for(idata=0;idata<dCnt;idata++)
-    {
-      hdDecodeData(LSWAP(outEvent->data[idata]));
-      /* if((idata%5)==0) printf("\n\t"); */
-      /* printf("  0x%08x ",(unsigned int)LSWAP(outEvent->data[idata])); */
-    }
-  printf("\n\n");
-
-  dmaPFreeItem(outEvent);
-
-}
-#endif
 
 /*
   Local Variables:
---
+++ test/readline_menu.h
@@ -0,0 +1,270 @@
+/* readline_menu.h - menu system routines using readline library. */
+
+/*
+ * This software uses the GNU Readline library, version 8-2, licensed
+ * under the GNU General Public License, version 3.  See
+ * https://www.gnu.org/software/readline/index.html for more
+ * information.
+ *
+ * This software also uses code from the following example in the GNU
+ * Readline library:
+ * https://tiswww.cwru.edu/php/chet/readline/readline.html#A-Short-Completion-Example
+ *
+ */
+
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/* A structure which contains information on the commands this program
+   can understand. */
+
+/* Forward declarations. */
+char *stripwhite();
+COMMAND *find_command();
+
+/* The name of this program, as taken from argv[0]. */
+char *progname;
+
+/* When non-zero, this global means the user is done using this program. */
+int done;
+
+char *
+dupstr(char *s)
+{
+  char *r;
+
+  r = malloc(strlen(s) + 1);
+  strcpy(r, s);
+  return (r);
+}
+
+/* Execute a command line. */
+int
+execute_line(char *line)
+{
+  register int i;
+  COMMAND *command;
+  char *word;
+
+  /* Isolate the command word. */
+  i = 0;
+  while(line[i] && whitespace(line[i]))
+    i++;
+  word = line + i;
+
+  while(line[i] && !whitespace(line[i]))
+    i++;
+
+  if(line[i])
+    line[i++] = '\0';
+
+  command = find_command(word);
+
+  if(!command)
+    {
+      fprintf(stderr, "%s: No such command\n", word);
+      return (-1);
+    }
+
+  /* Get argument to command, if any. */
+  while(whitespace(line[i]))
+    i++;
+
+  word = line + i;
+
+  /* Call the function. */
+  return ((*(command->func)) (word));
+}
+
+/* Look up NAME as the name of a command, and return a pointer to that
+   command.  Return a NULL pointer if NAME isn't a command name. */
+COMMAND *
+find_command(char *name)
+{
+  register int i;
+
+  for(i = 0; commands[i].name; i++)
+    if(strcmp(name, commands[i].name) == 0)
+      return (&commands[i]);
+
+  return ((COMMAND *) NULL);
+}
+
+/* Strip whitespace from the start and end of STRING.  Return a pointer
+   into STRING. */
+char *
+stripwhite(char *string)
+{
+  register char *s, *t;
+
+  for(s = string; whitespace(*s); s++)
+    ;
+
+  if(*s == 0)
+    return (s);
+
+  t = s + strlen(s) - 1;
+  while(t > s && whitespace(*t))
+    t--;
+  *++t = '\0';
+
+  return s;
+}
+
+/* **************************************************************** */
+/*                                                                  */
+/*                  Interface to Readline Completion                */
+/*                                                                  */
+/* **************************************************************** */
+
+char *command_generator PARAMS((const char *, int));
+char **fileman_completion PARAMS((const char *, int, int));
+
+/* Tell the GNU Readline library how to complete.  We want to try to complete
+   on command names if this is the first word in the line, or on filenames
+   if not. */
+void
+initialize_readline(char *name)
+{
+  /* Allow conditional parsing of the ~/.inputrc file. */
+  if(name)
+    rl_readline_name = dupstr(name);
+  else
+   rl_readline_name = "readline";
+
+  /* Tell the completer that we want a crack first. */
+  rl_attempted_completion_function = fileman_completion;
+}
+
+/* Attempt to complete on the contents of TEXT.  START and END bound the
+   region of rl_line_buffer that contains the word to complete.  TEXT is
+   the word to complete.  We can use the entire contents of rl_line_buffer
+   in case we want to do some simple parsing.  Return the array of matches,
+   or NULL if there aren't any. */
+char **
+fileman_completion(const char *text, int start, int end)
+{
+  char **matches;
+
+  matches = (char **) NULL;
+
+  /* If this word is at the start of the line, then it is a command
+     to complete.  Otherwise it is the name of a file in the current
+     directory. */
+  if(start == 0)
+    matches = rl_completion_matches(text, command_generator);
+
+  return (matches);
+}
+
+/* Generator function for command completion.  STATE lets us know whether
+   to start from scratch; without any state (i.e. STATE == 0), then we
+   start at the top of the list. */
+char *
+command_generator(const char *text, int state)
+{
+  static int list_index, len;
+  char *name;
+
+  /* If this is a new word to complete, initialize now.  This includes
+     saving the length of TEXT for efficiency, and initializing the index
+     variable to 0. */
+  if(!state)
+    {
+      list_index = 0;
+      len = strlen(text);
+    }
+
+  /* Return the next name which partially matches from the command list. */
+  while((name = commands[list_index].name) != NULL)
+    {
+      list_index++;
+
+      if(strncmp(name, text, len) == 0)
+	return (dupstr(name));
+    }
+
+  /* If no names matched, then return NULL. */
+  return ((char *) NULL);
+}
+
+
+/* Print out help for ARG, or for all of the commands if ARG is
+   not present. */
+int
+com_help(char *arg)
+{
+  register int i;
+  int printed = 0;
+
+  for(i = 0; commands[i].name; i++)
+    {
+      if(!*arg || (strcmp(arg, commands[i].name) == 0))
+	{
+	  printf("%s\t\t%s.\n", commands[i].name, commands[i].doc);
+	  printed++;
+	}
+    }
+
+  if(!printed)
+    {
+      printf("No commands match `%s'.  Possibilties are:\n", arg);
+
+      for(i = 0; commands[i].name; i++)
+	{
+	  /* Print in six columns. */
+	  if(printed == 6)
+	    {
+	      printed = 0;
+	      printf("\n");
+	    }
+
+	  printf("%s\t", commands[i].name);
+	  printed++;
+	}
+
+      if(printed)
+	printf("\n");
+    }
+  return (0);
+}
+
+/* The user wishes to quit using this program.  Just set DONE non-zero. */
+int
+com_quit(char *arg)
+{
+  done = 1;
+  return (0);
+}
+
+void
+readline_menu_loop(char *prompt)
+{
+  char *line, *s;
+
+  /* Loop reading and executing lines until the user quits. */
+  for(; done == 0;)
+    {
+      line = readline(prompt);
+
+      if(!line)
+	break;
+
+      /* Remove leading and trailing whitespace from the line.
+         Then, if there is anything left, add it to the history list
+         and execute it. */
+      s = stripwhite(line);
+
+      if(*s)
+	{
+	  add_history(s);
+	  execute_line(s);
+	}
+
+      free(line);
+    }
+
+}
