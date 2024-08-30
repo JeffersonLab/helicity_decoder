48aa49f5f3bd824823ebeb9ba35d46149904b854
Bryan Moffit <moffit@jlab.org>
2024-08-30 00:47:09 -0400

    add option arguments to configure program

--- systemd/hdConfigure.c
+++ systemd/hdConfigure.c
@@ -9,32 +9,66 @@
  */
 
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <getopt.h>
 #include "jvme.h"
 #include "hdLib.h"
 
-int hd_trigger_src = HD_INIT_FP;
-int hd_helicity_src = HD_INIT_EXTERNAL_FIBER;
-int hd_init_flag = 0;
+
+char *program_name;
+
+void
+usage()
+{
+  printf("\n\n");
+  printf("%s -a <A24 address> -t <Trigger Source> -h <Helicity Source>\n",
+	 program_name);
+  printf("\n");
+  printf("     -a <A24 address>               A24 of helicity decoder\n");
+  printf("                                    default: -a 0xed0000\n");
+  printf("\n");
+  printf("     -t <Trigger Source>            0: Internal\n");
+  printf("                                    1: Front Panel - default\n");
+  printf("                                    2: VXS\n");
+  printf("\n");
+  printf("     -h <Helicity Source>           0: Internal\n");
+  printf("                                    1: Fiber - default\n");
+  printf("                                    2: Copper\n");
+  printf("\n\n");
+
+}
 
 int
 main(int argc, char *argv[])
 {
 
   int stat = 0, rval = 0;
-  uint32_t address=0;
+  uint32_t address=0x00ed0000;
+  int hd_trigger_src = HD_INIT_FP;
+  int hd_helicity_src = HD_INIT_EXTERNAL_FIBER;
+  int hd_init_flag = 0;
+  int option;
 
-  if (argc > 1)
+  program_name = argv[0];
+
+  while((option = getopt(argc, argv, "a:t:h:")) != -1)
     {
-      address = (unsigned int) strtoll(argv[1],NULL,16)&0xffffffff;
-    }
-  else
-    {
-      address = 0x00ed0000; // my test module
+      switch (option)
+	{
+	case 'a':
+	  address = (uint32_t) strtoll(optarg,NULL,16)&0xffffffff;
+	  break;
+	case 't':
+	  hd_trigger_src = atoi(optarg);
+	  break;
+	case 'h':
+	  hd_helicity_src = atoi(optarg);
+	  break;
+	default:
+	  usage();
+	  exit(-1);
+	}
     }
 
   vmeSetQuietFlag(1);
--- systemd/hd_init.sh
+++ systemd/hd_init.sh
@@ -2,4 +2,16 @@
 
 export LINUXVME_LIB=/home/idaq/linuxvme/Linux-x86_64/lib
 export LD_LIBRARY_PATH=$LINUXVME_LIB
+
+# Run the program with option arguments
+# ./hdConfigure -a <A24 address> -t <Trigger Source> -h <Helicity Source>
+# -a <A24 address>               A24 of helicity decoder
+#                                default: -a 0xed0000
+# -t <Trigger Source>            0: Internal
+#                                1: Front Panel - default
+#                                2: VXS
+# -h <Helicity Source>           0: Internal
+#                                1: Fiber - default
+#                                2: Copper
+
 /home/idaq/linuxvme/helicity_decoder/systemd/hdConfigure
